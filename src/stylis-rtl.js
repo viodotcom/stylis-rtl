e3e2738147f42e3d82e489ed45af37a74836b23b
Evan Jacobs <probablyup@gmail.com>
2019-12-23 22:19:04 -0500

    fix shorthand flipping, handling of @noflip directives

--- src/stylis-rtl.js
+++ src/stylis-rtl.js
@@ -1,6 +1,6 @@
 // @flow
 
-import cssjanus from 'cssjanus'
+import cssjanus from "cssjanus";
 
 // https://github.com/thysultan/stylis.js#plugins
 const STYLIS_CONTEXTS = {
@@ -10,13 +10,16 @@ const STYLIS_CONTEXTS = {
   PROPERTY: 1,
   SELECTOR_BLOCK: 2,
   AT_RULE: 3
-}
+};
 
-export type StylisContextType = $Values<typeof STYLIS_CONTEXTS>
-export const STYLIS_PROPERTY_CONTEXT = STYLIS_CONTEXTS.PROPERTY
+export type StylisContextType = $Values<typeof STYLIS_CONTEXTS>;
+
+// We need to apply cssjanus as early as possible to capture the noflip directives if used
+// (they are not present at the PROPERTY, SELECTOR_BLOCK, or POST_PROCESS steps)
+export const STYLIS_PROPERTY_CONTEXT = STYLIS_CONTEXTS.PREPARATION;
 
 export default (context: StylisContextType, content: string): ?string => {
   if (context === STYLIS_PROPERTY_CONTEXT) {
-    return cssjanus.transform(content)
+    return cssjanus.transform(content);
   }
-}
+};
